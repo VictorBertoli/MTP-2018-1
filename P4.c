8f6494a9a6fa9799b27184de9641769450f85687
Victor Henrique Bértoli <37783549+VictorBertoli@users.noreply.github.com>
2018-05-09 14:19:08 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,17 @@
+//Victor Henrique Bértoli da Silva - 11721EBI015
+
+#include <stdio.h>
+
+int main(){
+	char num[256];
+	int n = 0, i;
+	printf("Digite uma string: ");
+	scanf("%s", &num);
+	for(i=0; num[i]!='\0'; i++){
+		if(num[i] >= '0' && num[i]<= '9'){
+			n = n*10 + (num[i] - '0');
+		}
+	}
+	printf("\n%d\n", n);
+	return 0;
+}
