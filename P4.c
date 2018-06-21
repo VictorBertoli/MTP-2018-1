4ce00c8f8e821f1bd1cb50dc336a1799d28be308
Victor Henrique Bértoli <37783549+VictorBertoli@users.noreply.github.com>
2018-06-21 09:22:49 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,4 +1,4 @@
-//Victor Henrique Bértoli da Silva - 11721EBI015
+//Victor Henrique BÃ©rtoli da Silva - 11721EBI015
 
 #include <stdio.h>
 
@@ -6,7 +6,7 @@ int main(){
 	char num[256];
 	int n = 0, i;
 	printf("Digite uma string: ");
-	scanf("%s", &num);
+	scanf("%s", num);
 	for(i=0; num[i]!='\0'; i++){
 		if(num[i] >= '0' && num[i]<= '9'){
 			n = n*10 + (num[i] - '0');
